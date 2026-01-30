db2f347d0423aa66d97abc4441e3a7d50475ba1d
alla-cherniauskaya <bezlikaa@gmail.com>
2026-01-30 13:45:22 +0300

    changes

---
+++ src/AppButton.vue
@@ -0,0 +1,32 @@
+<template>
+   <button 
+   class="btn" 
+   :class="color" 
+   :disabled="disabledButton"
+   @click.prevent="$emit(action)"><slot></slot></button>
+</template>
+
+<script>
+export default {
+  emits: ['upload-comments', 'add-new'],
+  props: {
+    color: {
+      type: String,
+      default: 'primary',
+    },
+    action: {
+      type: String,
+      required: true,
+    },
+    disabledButton: {
+      default: false,
+      type: Boolean,
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/AppUserResume.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card card-w70">
+    <div v-if="userData.length">
+      <div  v-for="element in userData" :key="element.id">
+        <h1 v-if="element.blockType === 'title'"> {{element.value}}</h1>
+        <div v-if="element.blockType === 'avatar'" class="avatar">
+          <img :src="element.value">
+        </div>
+        <h2 v-if="element.blockType === 'subtitle'"> {{element.value}}</h2>
+        <p v-if="element.blockType === 'text'">
+          {{element.value}}
+        </p>
+      </div>
+     
+    </div>
+   
+     <h3 v-else> Добавьте первый блок, чтобы увидеть результат</h3>
+   </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    userData: {
+      type: Array,
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
